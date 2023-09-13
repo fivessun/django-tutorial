e75fa14b9d872291913a56420228c0380b89e012
Taeyang Oh <ty610252@gmail.com>
2023-09-13 23:33:52 +0900

    django tutorial part-3

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,6 +2,11 @@ from django import urls
 
 from polls import views
 
+# For namespacing. If you use app_name, you have to annotate app name in html.
+app_name = "polls"
 urlpatterns = [
     urls.path("", views.index, name="index"),
+    urls.path("<int:question_id>/", views.detail, name="detail"),
+    urls.path("<int:question_id>/results/", views.results, name="results"),
+    urls.path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,35 @@
 from django import http
+from django import shortcuts
+from django.template import loader
+
+from polls import models
+
+
+def detail(request, question_id):
+    if True:  # Shortcut
+        question = shortcuts.get_object_or_404(models.Question, pk=question_id)
+    else:
+        try:
+            question = models.Question.objects.get(pk=question_id)
+        except models.Question.DoesNotExist:
+            raise http.Http404("Question does not exist")
+    return shortcuts.render(request, "polls/detail.html", {"question": question})
 
 
 def index(request):
-    return http.HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = models.Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    if True:  # Shortcut
+        return shortcuts.render(request, "polls/index.html", context)
+
+    template = loader.get_template("polls/index.html")
+    return http.HttpResponse(template.render(context, request))
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return http.HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return http.HttpResponse("You're voting on question %s." % question_id)
